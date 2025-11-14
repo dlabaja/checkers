128caad2fee004014eaa61478acca8bf547a3719
Ten dlabaja <jan.dlabaja01@upol.cz>
2025-11-14 09:23:37 +0100

    files

---
+++ Checkers/Piece.cs
@@ -0,0 +1,36 @@
+﻿namespace Checkers;
+
+public enum PieceType
+{
+    Pawn,
+    Queen
+}
+
+public enum PieceColor
+{
+    White,
+    Black
+}
+
+public class Piece
+{
+    private PieceType type;
+    private PieceColor color;
+    
+    public Piece(PieceType type, PieceColor color)
+    {
+        this.type = type;
+        this.color = color;
+    }
+
+    public bool Evolve()
+    {
+        if (this.type == PieceType.Pawn)
+        {
+            this.type = PieceType.Queen;
+            return true;
+        }
+
+        return false;
+    }
+}
