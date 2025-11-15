4fbf3ab0a43ae3344643f2127e2d1e8b319346f5
dlabaja <dlabaja.jan@seznam.cz>
2025-11-15 23:22:37 +0100

    findClosestToX

---
+++ Checkers/Utils.cs
@@ -0,0 +1,33 @@
+namespace Checkers;
+
+public static class Utils
+{
+    public static int FindClosestToX(int x, List<int> xList, bool returnHigher = true)
+    {
+        if (xList.Count == 1)
+        {
+            return xList[0];
+        }
+        
+        if (xList.Contains(x))
+        {
+            return x;
+        }
+
+        var closest = xList[0];
+        foreach (var item in xList)
+        {
+            if (Math.Abs(x - item) < Math.Abs(x - closest))
+            {
+                closest = item;
+            }
+        }
+
+        var second = 2 * x - closest;
+        if (xList.Contains(second))
+        {
+            return returnHigher ? Math.Max(closest, second) : Math.Min(closest, second);
+        }
+        return closest;
+    }
+}
